533da1fa9d56f2203077f5b25bea6ff51f54ab4f
Ivan Dardi <random-email@no-reply.com>
2019-06-02 09:25:28 -0300

    Improve generation script

--- db.py
+++ db.py
@@ -1,6 +1,26 @@
 import sqlite3
+import random
+import math
+
+QTD = 35
 
 db = sqlite3.connect('db.sqlite')
+
+instituicoes = ["USP", "UNICAMP", "UNIFESP", "FAPONE", "FAFOFO", "UNIVAP", "UFMG", "UFRJ"]
+
+
+def decode(i):
+    k = math.floor((1 + math.sqrt(1 + 8 * i)) / 2)
+    return k, i - k * (k - 1) // 2
+
+
+def rand_pair(n):
+    return decode(random.randrange(n * (n - 1) // 2))
+
+
+def rand_pairs(n, m):
+    return [decode(i) for i in random.sample(range(n * (n - 1) // 2), m)]
+
 
 with db:
     db.execute("""
@@ -54,26 +74,25 @@ with db:
         avaliacao INTEGER NOT NULL)
     """)
 
-    db.execute("""
-    INSERT INTO Aluno (nome, email, instituicao, credito, online)
-    VALUES
-        ("Nome 1", "nome1@email.com", "UNIFESP", 42.13, 0),
-        ("Nome 2", "nome2@email.com", "USP", 107.78, 1),
-        ("Nome 3", "nome3@email.com", "FATEC", 10.5, 1)
-    """)
+    for i in range(QTD):
+        db.execute("""
+        INSERT INTO Aluno (nome, email, instituicao, credito, online)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+                   ("Aluno {}".format(i), "aluno{}@email.com".format(i), random.choice(instituicoes),
+                    random.randrange(7.0, 350.0), random.randint(0, 1))
+                   )
     alunos = [id for (id,) in db.execute("SELECT id_aluno FROM Aluno")]
 
-
-
-    db.execute("""
-    INSERT INTO Monitor (nome, email, instituicao, descricao, reais_por_minuto, online)
-    VALUES
-        ("Monitor 1", "monitor1@email.com", "UNIVAP", "Oi Eu sou o Goku", 0.2, 1),
-        ("Monitor 2", "monitor2@email.com", "FAPONE", "Eu gosto de trens :|", 0.5, 0),
-        ("Monitor 3", "monitor3@email.com", "FAFOFO", "WEEEEEEEEEEEEEEEE", 0.35, 1)
-    """)
+    for i in range(QTD):
+        db.execute("""
+        INSERT INTO Monitor (nome, email, instituicao, descricao, reais_por_minuto, online)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+                   ("Monitor {}".format(i), "monitor{}@email.com".format(i), random.choice(instituicoes),
+                    "Terças e Quintas às 14h00", random.randrange(0.15, 0.50), random.randint(0, 1))
+                   )
     monitores = [id for (id,) in db.execute("SELECT id_monitor FROM Monitor")]
-
 
     db.execute("""
     INSERT INTO Topico (nome, materia)
@@ -87,41 +106,27 @@ with db:
     """)
     topicos = [id for (id,) in db.execute("SELECT id_topico FROM Topico")]
 
-    db.executemany("""
-    INSERT INTO TopicoAluno (id_aluno, id_topico)
-    VALUES (?, ?)
-    """,
-        [(alunos[0], topicos[0]),
-        (alunos[0], topicos[1]),
-        (alunos[0], topicos[2]),
-        (alunos[1], topicos[4]),
-        (alunos[2], topicos[1]),
-        (alunos[2], topicos[5])]
-    )
+    for a, b in rand_pairs(QTD, 75):
+        db.execute("""
+        INSERT INTO TopicoAluno (id_aluno, id_topico)
+        VALUES (?, ?)
+    """, (alunos[a], topicos[b]))
 
-    db.executemany("""
-    INSERT INTO TopicoMonitor (id_monitor, id_topico)
-    VALUES (?, ?)
-    """,
-        [(monitores[0], topicos[0]),
-        (monitores[0], topicos[2]),
-        (monitores[1], topicos[4]),
-        (monitores[1], topicos[5]),
-        (monitores[2], topicos[0]),
-        (monitores[2], topicos[3]),
-        (monitores[2], topicos[4]),
-        (monitores[2], topicos[5])]
-    )
+    for a, b in rand_pairs(QTD, 75):
+        db.execut("""
+        INSERT INTO TopicoMonitor (id_monitor, id_topico)
+        VALUES (?, ?)
+        """, (monitores[a], topicos[b]))
 
     db.executemany("""
     INSERT INTO Atendimento (id_aluno, id_monitor, id_topico, datetime_inicio, datetime_fim, avaliacao)
     VALUES (?, ?, ?, ?, ?, ?)
     """,
-        [(alunos[0], monitores[0], topicos[0], "2019-05-20 16:40:53", "2019-05-20 16:20:16", 9),
-        (alunos[0], monitores[0], topicos[2], "2019-05-25 09:15:37", "2019-05-25 09:55:56", 9.3),
-        (alunos[1], monitores[1], topicos[4], "2019-05-06 05:52:42", "2019-05-06 06:15:34", 10),
-        (alunos[1], monitores[2], topicos[4], "2019-05-13 17:24:15", "2019-05-13 17:32:36", 5),
-        (alunos[2], monitores[2], topicos[5], "2019-05-23 10:35:16", "2019-05-23 10:51:47", 7)]
-    )
+                   [(alunos[0], monitores[0], topicos[0], "2019-05-20 16:40:53", "2019-05-20 16:20:16", 9),
+                    (alunos[0], monitores[0], topicos[2], "2019-05-25 09:15:37", "2019-05-25 09:55:56", 9.3),
+                    (alunos[1], monitores[1], topicos[4], "2019-05-06 05:52:42", "2019-05-06 06:15:34", 10),
+                    (alunos[1], monitores[2], topicos[4], "2019-05-13 17:24:15", "2019-05-13 17:32:36", 5),
+                    (alunos[2], monitores[2], topicos[5], "2019-05-23 10:35:16", "2019-05-23 10:51:47", 7)]
+                   )
 
 db.close()
