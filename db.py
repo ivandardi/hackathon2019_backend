7898381d4238217d569a86f63f500e63b7f79e6d
Ivan Dardi <ivy.dardi@gmail.com>
2019-06-02 03:53:13 -0300

    Commit database

---
+++ db.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+db = sqlite3.connect('db.sqlite')
+
+db.execute("""
+INSERT INTO Aluno (nome, email, instituicao, credito, online)
+VALUES
+    ("Nome 1", "nome1@email.com", "UNIFESP", 42.13, 0),
+    ("Nome 2", "nome2@email.com", "USP", 107.78, 1),
+    ("Nome 3", "nome3@email.com", "FATEC", 10.5, 1)
+""")
+alunos = [id for (id,) in db.execute("SELECT id_aluno FROM Aluno")]
+
+
+
+db.execute("""
+INSERT INTO Monitor (nome, email, instituicao, descricao, reais_por_minuto, online)
+VALUES
+    ("Monitor 1", "monitor1@email.com", "UNIVAP", "Oi Eu sou o Goku", 0.2, 1),
+    ("Monitor 2", "monitor2@email.com", "FAPONE", "Eu gosto de trens :|", 0.5, 0),
+    ("Monitor 3", "monitor3@email.com", "FAFOFO", "WEEEEEEEEEEEEEEEE", 0.35, 1)
+""")
+monitores = [id for (id,) in db.execute("SELECT id_monitor FROM Monitor")]
+
+
+db.execute("""
+INSERT INTO Topico (nome, materia)
+VALUES
+    ("Segunda Revolucao Industrial", "Historia"),
+    ("Holocausto", "Historia"),
+    ("Terceira Guerra Mundial", "Historia"),
+    ("Funcoes Lineares", "Matematica"),
+    ("Sistemas Lineares", "Matematica"),
+    ("Derivadas", "Matematica")
+""")
+topicos = [id for (id,) in db.execute("SELECT id_topico FROM Topico")]
+
+db.executemany("""
+INSERT INTO TopicoAluno (id_aluno, id_topico)
+VALUES (?, ?)
+""",
+    [(alunos[0], topicos[0]),
+    (alunos[0], topicos[1]),
+    (alunos[0], topicos[2]),
+    (alunos[1], topicos[4]),
+    (alunos[2], topicos[1]),
+    (alunos[2], topicos[5])]
+)
+
+db.executemany("""
+INSERT INTO TopicoMonitor (id_monitor, id_topico)
+VALUES (?, ?)
+""",
+    [(monitores[0], topicos[0]),
+    (monitores[0], topicos[2]),
+    (monitores[1], topicos[4]),
+    (monitores[1], topicos[5]),
+    (monitores[2], topicos[0]),
+    (monitores[2], topicos[3]),
+    (monitores[2], topicos[4]),
+    (monitores[2], topicos[5])]
+)
+
+db.executemany("""
+INSERT INTO Atendimento (id_aluno, id_monitor, id_topico, datetime_inicio, datetime_fim, avaliacao)
+VALUES (?, ?, ?, ?, ?, ?)
+""",
+    [(alunos[0], monitores[0], topicos[0], "2019-05-20 16:40:53", "2019-05-20 16:20:16", 9),
+    (alunos[0], monitores[0], topicos[2], "2019-05-25 09:15:37", "2019-05-25 09:55:56", 9.3),
+    (alunos[1], monitores[1], topicos[4], "2019-05-06 05:52:42", "2019-05-06 06:15:34", 10),
+    (alunos[1], monitores[2], topicos[4], "2019-05-13 17:24:15", "2019-05-13 17:32:36", 5),
+    (alunos[2], monitores[2], topicos[5], "2019-05-23 10:35:16", "2019-05-23 10:51:47", 7)]
+)
+
+db.close()
---
+++ main.py
@@ -0,0 +1,117 @@
+from flask import Flask, jsonify, g, request
+import sqlite3
+from dateutil.parser import parse
+import time
+
+DATABASE = 'db.sqlite'
+
+app = Flask(__name__)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, *args):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return [{key: row[key] for key in row.keys()} for row in rv]
+
+
+@app.route("/aluno/<int:id_aluno>/")
+def api_aluno(id_aluno):
+    with app.app_context():
+        aluno = query_db("SELECT * FROM Aluno WHERE id_aluno = ?", id_aluno)[0]
+        topicos = query_db(
+            """SELECT Topico.id_topico, Topico.nome, Topico.materia FROM Topico, TopicoAluno, Aluno
+                WHERE TopicoAluno.id_topico = Topico.id_topico
+                  AND TopicoAluno.id_aluno = Aluno.id_aluno   
+                  AND Aluno.id_aluno = ?
+            """, id_aluno)
+        aluno["topicos"] = topicos
+        return jsonify(aluno)
+
+
+@app.route("/monitor/<int:id_monitor>/")
+def api_monitor(id_monitor):
+    with app.app_context():
+        monitor = query_db("SELECT * FROM Monitor WHERE id_monitor = ?", id_monitor)[0]
+        topicos = query_db(
+            """SELECT Topico.id_topico, Topico.nome, Topico.materia FROM Topico, TopicoMonitor, Monitor
+                WHERE TopicoMonitor.id_topico = Topico.id_topico
+                  AND TopicoMonitor.id_monitor = Monitor.id_monitor   
+                  AND Monitor.id_monitor = ?
+            """, id_monitor)
+        monitor["topicos"] = topicos
+        return jsonify(monitor)
+
+
+@app.route("/topicos/")
+def api_topicos():
+    with app.app_context():
+        aluno = query_db("SELECT * FROM Topico")
+        return jsonify(aluno)
+
+
+@app.route("/topico/<int:id_topico>/")
+def api_topico(id_topico):
+    with app.app_context():
+        aluno = query_db("SELECT * FROM Topico WHERE id_topico = ?", id_topico)[0]
+        return jsonify(aluno)
+
+
+# POST precisa de: id_aluno, id_monitor, id_topico, datetime_inicio, datetime_fim, avaliacao
+@app.route("/atendimentos/", methods=["GET", "POST"])
+def api_atendimentos():
+    if request.method == "POST":
+        with app.app_context():
+            query_db(
+                "INSERT INTO Atendimento (id_aluno, id_monitor, id_topico, datetime_inicio, datetime_fim, avaliacao) VALUES (?, ?, ?, ?, ?, ?)",
+                request.form["id_aluno"],
+                request.form["id_monitor"],
+                request.form["id_topico"],
+                request.form["datetime_inicio"],
+                request.form["datetime_fim"],
+                request.form["avaliacao"],
+                )
+
+            dt_inicio = parse(request.form["datetime_inicio"])
+            dt_fim = parse(request.form["datetime_fim"])
+
+            unix_inicio = time.mktime(dt_inicio.timetuple())
+            unix_fim = time.mktime(dt_fim.timetuple())
+
+            tempo_total = int(unix_fim - unix_inicio) / 60
+
+            rpm = query_db("SELECT reais_por_minuto FROM Monitor WHERE id_monitor = ?", request.form["id_monitor"])
+
+            custo_total = tempo_total * rpm
+
+            query_db("UPDATE Aluno SET credito = credito - ? WHERE id_aluno = ?", custo_total, request.form["id_aluno"])
+    else:
+        with app.app_context():
+            atendimento = query_db("SELECT * FROM Atendimento")
+            return jsonify(atendimento)
+
+
+@app.route("/atendimento/<int:id_atendimento>/")
+def api_atendimento(id_atendimento):
+    with app.app_context():
+        atendimento = query_db("SELECT * FROM Topico WHERE id_atendimento = ?", id_atendimento)[0]
+        return jsonify(atendimento)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
